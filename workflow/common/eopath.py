4685fde00da55d002d5078d7cd256f2471209eb6
Martin de Graaf <martin.de.graaf@knmi.nl>
2026-03-19 11:16:12 +0100

    Delete workflow/common directory

--- workflow/common/eopath.py
+++
@@ -1,7 +0,0 @@
-#Python helper functions to retrieve platform dependent paths etc.
-
-import os
-
-def get_S1_datapath():
-    """dummy function to load a platform dependent data path"""
-    return os.path.join('data','s1')
